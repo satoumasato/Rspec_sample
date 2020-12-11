4a21041c5b6e5f9ed60c86076727c9888047f231
satoumasato <masakun1993@gmail.com>
2020-12-11 23:21:33 +0900

    rspec編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   attr_accessor :name, :age
 
   def initialize(name:, age:)
-    self.name = name
-    self.age = age
+     self.name = name
+     self.age = age
   end
 
   def disp_name
